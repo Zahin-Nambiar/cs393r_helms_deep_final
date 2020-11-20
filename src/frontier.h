bfd0bb877cf35b3830f912214227c107e127f004
Zahin-Nambiar <znambi@utexas.edu>
2020-11-19 18:22:18 -0600

    upload starter files

---
+++ src/frontier.h
@@ -0,0 +1,19 @@
+#include <vector>
+#include <list> 
+
+namespace frontiers{
+
+namespace ros {
+  class NodeHandle;
+}  // namespace ros
+
+
+class Frontier {
+    public:
+    private:
+
+
+
+};
+
+}
