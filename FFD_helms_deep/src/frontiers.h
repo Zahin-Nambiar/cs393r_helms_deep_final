a531045a1609b3246bdff61ad137be779e429fdd
Zahin-Nambiar <znambi@utexas.edu>
2020-11-21 16:29:32 -0600

    fixed namespace issues in frontiers.h

--- FFD_helms_deep/src/frontiers.h
+++ FFD_helms_deep/src/frontiers.h
@@ -18,18 +18,18 @@ namespace FFD{
     };
 
     struct frontier_DB{
-        std::vector<frontier> frontiers
+        std::vector<frontier> frontiers;
     };
 
     class Frontier {
         public:
             //Appends new frontiers from a contour
-            void ExtractNewFrontier(constant Contour c,frontier_DB* new_frontiers_ptr);
+            void ExtractNewFrontier(const Contour c,frontier_DB* new_frontiers_ptr);
             
             
-            void MaintainFrontiers(frontier_DB* database_ptr, frontier_DB* new_frontiers_ptr, const vector<Vector2f>* active_area_ptr);
+            void MaintainFrontiers(frontier_DB* database_ptr, frontier_DB* new_frontiers_ptr, const std::vector<Eigen::Vector2f>* active_area_ptr);
             //Does part of new frontier overlap with an existing frontier in the database?
-            void SplitFrontier(frontier_DB* database_ptr, frontier* new_frontier_ptr, constant float split);
+            void SplitFrontier(frontier_DB* database_ptr, frontier* new_frontier_ptr, const float split);
             void RemoveFrontier(frontier_DB* database_ptr, frontier* new_frontier_ptr);
             bool ExistFrontier(frontier_DB* database_ptr, frontier* new_frontier_ptr);
             void MergeFrontiers(frontier* a_ptr,frontier* b_ptr);
@@ -37,7 +37,7 @@ namespace FFD{
 
 
             //Frontier is a list of points, the robot goal is the average of the frontiers points. The closest average is the frontier average to go. 
-            void ReturnClosestFrontierAverage(frontier_DB* databse_ptr,vector<float>* robot_pose_ptr,vector<float>* nav_goal_ptr )
+            void ReturnClosestFrontierAverage(frontier_DB* databse_ptr,std::vector<float>* robot_pose_ptr,std::vector<float>* nav_goal_ptr );
         private:
             
 
